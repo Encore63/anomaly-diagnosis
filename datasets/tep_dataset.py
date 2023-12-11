a2350d964d72b1df410c30ea09a4f0bec5e98ee3
YoRHa <511032218@qq.com>
2023-12-11 12:18:52 +0800

    Initial commit

---
+++ datasets/tep_dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data.dataset import Dataset
+
+
+class TEPDataset(Dataset):
+    def __init__(self):
+        ...
+
+    def __getitem__(self, index):
+        ...
+
+    def __len__(self):
+        ...
