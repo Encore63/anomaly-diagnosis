8c9f6108765bb6d449a4e94c07ac637d39cfe9c0
YoRHa <511032218@qq.com>
2024-01-18 18:09:55 +0800

    Update project.

--- utils/visualize.py
+++ utils/visualize.py
@@ -1,2 +1,50 @@
-import sklearn
+import itertools
+import numpy as np
 import seaborn as sns
+
+from matplotlib import pyplot as plt
+
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    """
+    this function prints and plots the confusion matrix.
+    Normalization can be applied by setting 'normalize=True'.
+    input
+    - cm: 计算出的混淆矩阵的值
+    - classes : 混淆矩阵中每一行每一列对应的列
+    - normalize: true:显示百分比， false: 显示个数
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=90)
+    plt.yticks(tick_marks, classes)
+
+    plt.axis("equal")
+    ax = plt.gca()
+    left, right = plt.xlim()
+    ax.spines['left'].set_position(('data', left))
+    ax.spines['right'].set_position(('data', right))
+    for edge_i in ['top', 'bottom', 'right', 'left']:
+        ax.spines[edge_i].set_edgecolor('white')
+
+    #
+    thresh = cm.max() / 2
+    print("thresh", thresh.dtype)
+
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        num = '{:.2f}'.format(cm[i, j]) if normalize else int(cm[i, j])
+        plt.text(j, i, num,
+                 verticalalignment='center',
+                 horizontalalignment="center",
+                 color="white" if float(num) > thresh else "black")
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
